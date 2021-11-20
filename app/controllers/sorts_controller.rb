750e3fdae07ba70177ba8028952524e2a3159fa7
pln9abe <bernabe.cambarihanjr@gmail.com>
2021-11-20 16:00:12 +0800

    quicksort method

--- app/controllers/sorts_controller.rb
+++ app/controllers/sorts_controller.rb
@@ -1,5 +1,6 @@
 class SortsController < ApplicationController
   BUBBLE_SORT = 'Bubble Sort'
+  FIRST = 0
 
   def index
     @sorts = Sort.all
@@ -26,25 +27,56 @@ class SortsController < ApplicationController
     end
 
     def check_sorting_method
-      return bubble_sort if @sort.sorting_method == BUBBLE_SORT
-      quick_sort 
+      return bubble_sort(array_string, word_length) if @sort.sorting_method == BUBBLE_SORT
+      quick_sort(array_string, FIRST, word_length - 1)
     end
 
-    def bubble_sort
-      array_string = @sort.word.split('')
-      word_length = @sort.word.size
+    def array_string
+      @sort.word.split('')
+    end
 
+    def word_length
+      @sort.word.size
+    end
+
+    def bubble_sort(array, size)
       loop do
         swapped = false
 
-        (word_length - 1).times do |idx|
-          if array_string[idx] > array_string[idx+1]
-            array_string[idx], array_string[idx+1] = array_string[idx+1], array_string[idx]
+        (size - 1).times do |idx|
+          if array[idx] > array[idx+1]
+            array[idx], array[idx+1] = array[idx+1], array[idx]
             swapped = true
           end
         end
         break if not swapped
       end
-      @sort.word = array_string.join('')
+      @sort.word = array.join('')
+    end
+
+    def quick_sort(array, first, last)
+      if first < last
+        j = partition(array, first, last)
+        quick_sort(array, first, j-1)
+        quick_sort(array, j+1, last)
+      end
+      @sort.word = array.join('')
+    end
+
+    def partition(array, first, last)
+      pivot = array[last]
+      pIndex = first
+      i = first
+
+      while i < last
+        if array[i] <= pivot
+          array[i], array[pIndex] = array[pIndex], array[i]
+          pIndex += 1
+        end
+        i += 1
+      end
+
+      array[pIndex], array[last] = array[last], array[pIndex]
+      return pIndex
     end
 end
